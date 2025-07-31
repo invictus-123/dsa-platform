247cc94aef180ca7a212f87351f3c2d97787e6bb
invictus-123 <ayushman.mathur7@gmail.com>
2025-07-31 23:09:25 +0530

    Add integration tests for submission APIs

--- backend/src/test/java/com/online/judge/backend/integration/LifecycleIT.java
+++ backend/src/test/java/com/online/judge/backend/integration/LifecycleIT.java
@@ -6,14 +6,23 @@ import com.online.judge.backend.dto.request.CreateProblemRequest;
 import com.online.judge.backend.dto.request.CreateTestCaseRequest;
 import com.online.judge.backend.dto.request.LoginRequest;
 import com.online.judge.backend.dto.request.RegisterRequest;
+import com.online.judge.backend.dto.request.SubmitCodeRequest;
 import com.online.judge.backend.dto.response.AuthResponse;
 import com.online.judge.backend.dto.response.CreateProblemResponse;
 import com.online.judge.backend.dto.response.GetProblemByIdResponse;
+import com.online.judge.backend.dto.response.GetSubmissionByIdResponse;
 import com.online.judge.backend.dto.response.ListProblemsResponse;
+import com.online.judge.backend.dto.response.ListSubmissionsResponse;
+import com.online.judge.backend.dto.response.SubmitCodeResponse;
 import com.online.judge.backend.dto.ui.ProblemDetailsUi;
 import com.online.judge.backend.dto.ui.ProblemSummaryUi;
+import com.online.judge.backend.dto.ui.SubmissionDetailsUi;
+import com.online.judge.backend.dto.ui.SubmissionSummaryUi;
+import com.online.judge.backend.dto.ui.UserSummaryUi;
 import com.online.judge.backend.model.shared.ProblemDifficulty;
 import com.online.judge.backend.model.shared.ProblemTag;
+import com.online.judge.backend.model.shared.SubmissionLanguage;
+import com.online.judge.backend.model.shared.SubmissionStatus;
 import com.online.judge.backend.model.shared.UserRole;
 import java.util.List;
 import java.util.UUID;
@@ -64,6 +73,9 @@ class LifecycleIT {
 			PROBLEM_MEMORY_LIMIT,
 			PROBLEM_TAGS,
 			PROBLEM_TEST_CASES);
+	private static final int SUBMISSION_COUNT = 25;
+	private static final SubmissionLanguage SUBMISSION_LANGUAGE = SubmissionLanguage.CPP;
+	private static final String CODE = "void main() {}";
 
 	@Container
 	private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
@@ -80,15 +92,18 @@ class LifecycleIT {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	private static String userHandle;
 	private static String userToken;
+	private static String adminHandle;
 	private static String adminToken;
 	private static Long problemId;
+	private static Long submissionId;
 
 	@Test
 	@Order(1)
 	void userAndAdminRegistrationAndLogin() {
 		// 1. Register a standard USER
-		String userHandle = ("testuser-" + UUID.randomUUID()).substring(0, 20);
+		userHandle = ("testuser-" + UUID.randomUUID()).substring(0, 20);
 		String userEmail = "testuser-" + UUID.randomUUID() + "@example.com";
 		String password = "password123";
 		RegisterRequest userRegisterRequest =
@@ -107,7 +122,7 @@ class LifecycleIT {
 		assertThat(userToken).isNotBlank();
 
 		// 3. Register an ADMIN and retrieve the JWT
-		String adminHandle = ("testadmin-" + UUID.randomUUID()).substring(0, 20);
+		adminHandle = ("testadmin-" + UUID.randomUUID()).substring(0, 20);
 		String adminEmail = "testadmin-" + UUID.randomUUID() + "@example.com";
 		RegisterRequest adminRegisterRequest =
 				new RegisterRequest(adminHandle, adminEmail, password, "Admin", "User", UserRole.ADMIN);
@@ -181,6 +196,58 @@ class LifecycleIT {
 
 	@Test
 	@Order(4)
+	void onlyLoggedInUsersCanMakeSubmissions() {
+		SubmitCodeRequest submitCodeRequest = new SubmitCodeRequest(problemId, CODE, SUBMISSION_LANGUAGE);
+		ResponseEntity<SubmitCodeResponse> submitCodeResponse =
+				restTemplate.postForEntity(BASE_URL + "/submissions", submitCodeRequest, SubmitCodeResponse.class);
+
+		assertThat(submitCodeResponse.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+
+		HttpHeaders userHeaders = new HttpHeaders();
+		userHeaders.setBearerAuth(userToken);
+		HttpEntity<SubmitCodeRequest> userSubmitCodeRequest = new HttpEntity<>(submitCodeRequest, userHeaders);
+		ResponseEntity<SubmitCodeResponse> userSubmitCodeResponse =
+				restTemplate.postForEntity(BASE_URL + "/submissions", userSubmitCodeRequest, SubmitCodeResponse.class);
+
+		assertThat(userSubmitCodeResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		SubmissionDetailsUi submissionDetails = userSubmitCodeResponse.getBody().submissionDetails();
+		assertSubmissionDetails(submissionDetails);
+		submissionId = submissionDetails.id();
+
+		// Create additional submissions to test the list and get APIs
+		for (int i = 2; i <= SUBMISSION_COUNT; i++) {
+			userSubmitCodeResponse = restTemplate.postForEntity(
+					BASE_URL + "/submissions", userSubmitCodeRequest, SubmitCodeResponse.class);
+			assertThat(userSubmitCodeResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		}
+	}
+
+	@Test
+	@Order(5)
+	void retrieveSubmissions_withoutJwtHeaders() {
+		ResponseEntity<ListSubmissionsResponse> listResponse =
+				restTemplate.getForEntity(BASE_URL + "/submissions/list", ListSubmissionsResponse.class);
+
+		assertThat(listResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		List<SubmissionSummaryUi> submissions = listResponse.getBody().submissions();
+		// 20 is the page size
+		assertThat(submissions).isNotNull().hasSize(20);
+
+		listResponse = restTemplate.getForEntity(BASE_URL + "/submissions/list?page=2", ListSubmissionsResponse.class);
+
+		assertThat(listResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		submissions = listResponse.getBody().submissions();
+		assertThat(submissions).isNotNull().hasSize(5);
+
+		ResponseEntity<GetSubmissionByIdResponse> submissionDetailsResponse =
+				restTemplate.getForEntity(BASE_URL + "/submissions/" + submissionId, GetSubmissionByIdResponse.class);
+
+		assertThat(submissionDetailsResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertSubmissionDetails(submissionDetailsResponse.getBody().submissionDetails());
+	}
+
+	@Test
+	@Order(6)
 	void logoutUsers() {
 		HttpHeaders userHeaders = new HttpHeaders();
 		userHeaders.setBearerAuth(userToken);
@@ -208,5 +275,27 @@ class LifecycleIT {
 		assertThat(problemDetails.memoryLimitInMb()).isEqualTo(PROBLEM_MEMORY_LIMIT);
 		assertThat(problemDetails.tags()).containsExactlyInAnyOrderElementsOf(PROBLEM_TAGS);
 		assertThat(problemDetails.sampleTestCases()).hasSize(1);
+	}
+
+	private void assertProblemSummary(ProblemSummaryUi problemSummary) {
+		assertThat(problemSummary).isNotNull();
+		assertThat(problemSummary.title()).isEqualTo(PROBLEM_TITLE);
+		assertThat(problemSummary.difficulty()).isEqualTo(PROBLEM_DIFFICULTY);
+		assertThat(problemSummary.tags()).containsExactlyInAnyOrderElementsOf(PROBLEM_TAGS);
+	}
+
+	private void assertSubmissionDetails(SubmissionDetailsUi submissionDetails) {
+		assertThat(submissionDetails).isNotNull();
+		assertProblemSummary(submissionDetails.problemSummary());
+		assertUserSummary(submissionDetails.userSummary());
+		assertThat(submissionDetails.userSummary()).isNotNull();
+		assertThat(submissionDetails.status()).isEqualTo(SubmissionStatus.WAITING_FOR_EXECUTION);
+		assertThat(submissionDetails.language()).isEqualTo(SUBMISSION_LANGUAGE);
+		assertThat(submissionDetails.code()).isEqualTo(CODE);
+	}
+
+	private void assertUserSummary(UserSummaryUi userSummary) {
+		assertThat(userSummary).isNotNull();
+		assertThat(userSummary.handle()).isEqualTo(userHandle);
 	}
 }
